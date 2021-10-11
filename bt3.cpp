f3cc41abd0167b241c6732c256d7433fa37e281b
unknown <khuongndth2004001@fpt.edu.vn>
2021-10-11 13:25:42 +0700

    than thien nguoi dung

--- bt3.cpp
+++ bt3.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main(){
     int n;
     float S=0;
+    cout<<"Nhap 1 so nguyen duong n: \n";
     cin>>n;
     for(int i=1;i<=n;i++){
         S+=(1/(float)i);
     }
-    cout<<S;
+    cout<<"Gia tri cua S la: "<<S;
 
 }
--- bt6.cpp
+++ bt6.cpp
@@ -1,15 +1,16 @@
-//Tinh 1/1.2+1/2.3+...+1/n(n+1)
+//Tinh S=1/1.2+1/2.3+...+1/n(n+1)
 
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     float S=0;
+    cout<<"Nhap 1 so nguyen duong n: \n";
     cin>>n;
     for(int i=1;i<=n;i++){
         S+=(1/(float)(i*(i+1)));
     }
-    cout<<S;
+    cout<<"Gia tri tong S la: "<<S;
 
 //Cách đi nhanh: cout<<(1-1/(n+1)); :))
 
